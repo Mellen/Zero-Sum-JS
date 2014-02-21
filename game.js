fee30f2ea7c93ef992e6d75b409c52ad22de8874
Matt Ellen <matt@matthewellen.co.uk>
2014-02-21 23:16:47 +0000

    display board, score and next number when the game starts random number 1 -> 9 method

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
 function game(boardview, scoreview, nextview)
 {
+    function rnd1to9()
+    {
+	return Math.ceil(Math.random()*9);
+    }
+
     var boardview = boardview;
     var scoreview = scoreview;
     var nextview = nextview;
@@ -7,10 +12,40 @@ function game(boardview, scoreview, nextview)
     var board = [];
     var rows = 23;
     var cols = 6;
-    for(var col = 0; col < cols; coll++)
+    var next = rnd1to9();
+    var current = rnd1to9();
+
+    for(var col = 0; col < cols; col++)
     {
+	board.push([]);
 	for(var row = 0; row < rows; row++)
 	{
+	    board[col].push(-1);
 	}
     }
+
+    function start()
+    {
+	draw();
+    }
+
+    this.start = start;
+
+    function draw()
+    {
+	nextview.textContent = next;
+	scoreview.textContent = score;
+	for(var col = 0; col < cols; col++)
+	{
+	    for(var row = 0; row < rows; row++)
+	    {
+		if(board[col][row] != -1)
+		{
+		    boardview.rows[row].cols[col].textContent = board[col][row];
+		}
+	    }
+	}
+	
+    }
+
 }
